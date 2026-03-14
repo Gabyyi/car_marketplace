f5c36ee280fdd637dc329583d018084d36de4bf7
Gabyyi <ionutgabriel.pruteanu@gmail.com>
2026-03-14 20:57:08 +0200

    updated home page

---
+++ frontend/src/components/SwipeCar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="w-64 snap-start shrink-0 rounded-xl bg-white p-3">
+    <div class="aspect-4/3 overflow-hidden rounded-md bg-gray-100">
+      <img :src="car.image" :alt="car.make + ' ' + car.model" class="h-full w-full object-cover object-center" />
+    </div>
+
+    <div class="mt-3">
+      <div class="text-sm font-medium text-gray-900">{{ car.make }} {{ car.model }}</div>
+      <div class="mt-1 text-lg font-semibold text-gray-900">€{{ car.price }}</div>
+
+      <div class="mt-2 flex flex-wrap gap-2 text-xs">
+        <span v-for="(t, i) in tagList" :key="i" class="rounded-full bg-gray-100 px-2 py-1 text-gray-700">{{ t }}</span>
+      </div>
+
+      <div class="mt-8 text-sm text-gray-600">
+        <i class="pi pi-map-marker mr-1 text-blue-600"></i>{{ car.location }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwipeCar',
+  props: {
+    car: {
+      type: Object,
+      default: () => ({
+        image: 'https://images.unsplash.com/photo-1493238792000-8113da705763?auto=format&fit=crop&w=800&q=80',
+        make: 'BMW',
+        model: 'M3',
+        price: '45,000',
+        fuelType: 'Petrol',
+        gearbox: 'Automatic',
+        power: '375hp',
+        kilometers: '50,000 km',
+        year: '2019',
+        location: 'Bucharest'
+      })
+    }
+  },
+  computed: {
+    tagList() {
+      return [this.car.year, this.car.kilometers, this.car.power, this.car.gearbox, this.car.fuelType].filter(Boolean)
+    }
+  }
+}
+</script>
--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -131,14 +131,59 @@
     </article>
 
     <CarAdCard />
+
+    <article class="rounded-3xl border border-gray-200 bg-white p-5 shadow-sm sm:p-6 md:p-8">
+      <h2 class="text-3xl font-semibold text-gray-900">Top deals for you</h2>
+      <div class="mt-4 relative">
+        <div ref="swipeContainer" class="-mx-3 flex gap-4 overflow-x-auto px-3 py-2 snap-x snap-mandatory touch-pan-x">
+          <SwipeCar v-for="(c, idx) in swipeCars" :key="idx" :car="c" />
+        </div>
+
+        <div class="absolute right-2 top-1/2 hidden -translate-y-1/2 md:block">
+          <button @click="scrollNext" class="rounded-full bg-white/90 p-2 shadow-md hover:bg-white">
+            <i class="pi pi-chevron-right"></i>
+          </button>
+        </div>
+        <div class="absolute left-2 top-1/2 hidden -translate-y-1/2 md:block">
+          <button @click="scrollPrev" class="rounded-full bg-white/90 p-2 shadow-md hover:bg-white">
+            <i class="pi pi-chevron-left"></i>
+          </button>
+        </div>
+      </div>
+    </article>
   </section>
 </template>
 
 <script>
+import { ref } from 'vue'
 import CarAdCard from '../components/CarAdCard.vue'
+import SwipeCar from '../components/SwipeCar.vue'
 
 export default {
   name: 'HomePage',
-  components: { CarAdCard }
+  components: { CarAdCard, SwipeCar },
+  setup() {
+    const swipeContainer = ref(null)
+
+    const swipeCars = [
+      { image: 'https://images.unsplash.com/photo-1493238792000-8113da705763?auto=format&fit=crop&w=800&q=80', make: 'BMW', model: 'M3', price: '45,000', fuelType: 'Petrol', gearbox: 'Automatic', power: '375hp', kilometers: '50,000 km', year: '2019', location: 'Bucharest' },
+      { image: 'https://images.unsplash.com/photo-1502877338535-766e1452684a?auto=format&fit=crop&w=800&q=80', make: 'Audi', model: 'A4', price: '29,500', fuelType: 'Diesel', gearbox: 'Manual', power: '190hp', kilometers: '85,000 km', year: '2018', location: 'Cluj' },
+      { image: 'https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?auto=format&fit=crop&w=800&q=80', make: 'Mercedes-Benz', model: 'C-Class', price: '34,200', fuelType: 'Petrol', gearbox: 'Automatic', power: '220hp', kilometers: '60,000 km', year: '2020', location: 'Timisoara' },
+      { image: 'https://images.unsplash.com/photo-1504215680853-026ed2a45def?auto=format&fit=crop&w=800&q=80', make: 'Volkswagen', model: 'Golf', price: '15,000', fuelType: 'Diesel', gearbox: 'Manual', power: '150hp', kilometers: '120,000 km', year: '2017', location: 'Iasi' },
+      { image: 'https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?auto=format&fit=crop&w=800&q=80', make: 'Ford', model: 'Focus', price: '12,500', fuelType: 'Petrol', gearbox: 'Manual', power: '125hp', kilometers: '90,000 km', year: '2016', location: 'Constanta' }
+    ]
+
+    function scrollNext() {
+      if (!swipeContainer.value) return
+      swipeContainer.value.scrollBy({ left: swipeContainer.value.clientWidth * 0.7, behavior: 'smooth' })
+    }
+
+    function scrollPrev() {
+      if (!swipeContainer.value) return
+      swipeContainer.value.scrollBy({ left: -swipeContainer.value.clientWidth * 0.7, behavior: 'smooth' })
+    }
+
+    return { swipeContainer, swipeCars, scrollNext, scrollPrev }
+  }
 }
 </script>
